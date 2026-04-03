70e5812b89481f3b5b62047f9787bd0463e0a1bd
Jitendra Prajapati <jitendraprajapati9956@gmail.com>
2026-04-03 16:24:05 +0530

    fix merge conflict

--- routes/entryRoutes.js
+++ routes/entryRoutes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express");
 const router = express.Router();
 
@@ -15,22 +14,4 @@ router.post("/add", createEntry);
 router.delete("/:id", deleteEntry);
 router.put("/:id", updateEntry);
 
-=======
-const express = require("express");
-const router = express.Router();
-
-const {
-  createEntry,
-  getEntries,
-  deleteEntry,
-  updateEntry
-} = require("../controllers/entryController");
-
-// ✅ Routes
-router.get("/all", getEntries);
-router.post("/add", createEntry);
-router.delete("/:id", deleteEntry);
-router.put("/:id", updateEntry);
-
->>>>>>> 4d15e83 (Vercel-ready serverless Express setup)
 module.exports = router;
