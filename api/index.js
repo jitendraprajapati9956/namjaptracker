9487a4620cb0a1a8e6cedeb564356ddb48e808c5
JITENDRA PRAJAPATI <115032780+jitendraprajapati9956@users.noreply.github.com>
2026-04-03 15:22:41 +0530

    Set up Express app for serverless deployment
    Replaced the previous server export with an Express app configured for serverless deployment.

--- api/index.js
+++ api/index.js
@@ -1,3 +1,11 @@
-const app = require("../server");
+const express = require('express');
+const serverless = require('serverless-http'); // install via npm i serverless-http
+const app = express();
 
-module.exports = app;
+// Define your routes
+app.get('/', (req, res) => {
+  res.send('Hello from Vercel Express!');
+});
+
+// Export as serverless function
+module.exports = serverless(app);
