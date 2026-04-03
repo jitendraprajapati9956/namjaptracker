fff48354f6e583d623d91d9d483e5031d3886e98
Jitendra Prajapati <jitendraprajapati9956@gmail.com>
2026-04-03 16:12:37 +0530

    Fix Vercel routing and serverless setup

--- api/index.js
+++ api/index.js
@@ -10,8 +10,4 @@ app.get("/", (req, res) => {
   res.send("✅ Namjap Tracker API is working!");
 });
 
-// Your existing routes
-const entryRoutes = require("../routes/entryRoutes");
-app.use("/api/entries", entryRoutes);
 module.exports.handler = serverless(app);
-module.exports = app;
